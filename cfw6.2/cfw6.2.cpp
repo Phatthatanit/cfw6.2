9c1ddf7b3e864f75f061ff6312107ec0e89be6ae
PHATTHARANIT TESJAROEN <63010727@KMITL.AC.TH>
2020-09-18 22:57:07 +0700

    Add project files.

---
+++ cfw6.2/cfw6.2.cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+char name[100];
+int c;
+void x() {
+	printf("How many letters of your name  : ");
+	scanf("%d", &c);
+	for (int i = 1; i <= c; i++)
+	{
+		printf("letters %d of your name  : ", i);
+		scanf("%s", &name[i]);
+
+	}
+	for (int j = c; j >= 1; j--)
+	{
+		for (int z = c; z >= 1; z--)
+		{
+
+			printf("%c", name[j]);
+		}
+		printf("\n");
+	}
+}
+
+int main() {
+	x();
+	return 0;
+}
